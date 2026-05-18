1bd022119d18b2e4c489aea2e14f5b1e0e38e349
highsilkhat <pl4400@protonmail.com>
2026-05-17 19:27:24 -0700

    adding vercel analytics dependency and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import BurgerMenu from './components/BurgerMenu.jsx'
 import Home from './components/Home.jsx'
@@ -20,7 +21,9 @@ export default function App() {
                     <Route path="/events" element={<Events />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
+                <Analytics />
             </main>
         </Router>
+
     )
 }
